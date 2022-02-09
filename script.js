fec1ec2e12ba72515f07c92f6f22d9e049f305e5
Haribaskar619 <79138852+Haribaskar619@users.noreply.github.com>
2022-02-09 17:15:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//  Fix the bugs in below snippet 
+//  <html lang="en">
+//     <head>
+//         <title>Document
+//             <body>
+//                 guvi
+//         </title>
+//     </head>
+//     <div>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//         <div>
+//             <div>
+//                 Guvi Geek Network
+//             </div>
+//         </body>
+//     </html> 
+//  Body  element misplaced  inside head, / missing in first div closing 
+
+//  code after fixing the bugs 
+//  <html lang="en">
+//     <head>
+//         <title>Document 
+//         </title>
+//     </head>
+//     <body>
+//         <h1>Guvi</h1>
+//     <div>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//     </div>
+//             <div>
+//                 Guvi Geek Network
+//             </div>
+//     </body>
+//     </html> 
+
